ad9c334b4a5288bcd34c8fe4bb72026d8911b7be
twain young <twainyoung@gmail.com>
2020-02-09 16:07:53 +0800

    Site updated: 2020-02-09 16:07:52

--- js/search.js
+++ js/search.js
@@ -1,36 +1,36 @@
-var searchFunc = function (path, search_id, content_id) {
+let searchFunc = function (path, search_id, content_id) {
     'use strict';
     $.ajax({
         url: path,
         dataType: "xml",
         success: function (xmlResponse) {
             // get the contents from search data
-            var datas = $("entry", xmlResponse).map(function () {
+            let datas = $("entry", xmlResponse).map(function () {
                 return {
                     title: $("title", this).text(),
                     content: $("content", this).text(),
                     url: $("url", this).text()
                 };
             }).get();
-            var $input = document.getElementById(search_id);
-            var $resultContent = document.getElementById(content_id);
+            let $input = document.getElementById(search_id);
+            let $resultContent = document.getElementById(content_id);
             $input.addEventListener('input', function () {
-                var str = '<ul class=\"search-result-list\">';
-                var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
+                let str = '<ul class=\"search-result-list\">';
+                let keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
                 $resultContent.innerHTML = "";
                 if (this.value.trim().length <= 0) {
                     return;
                 }
                 // perform local searching
                 datas.forEach(function (data) {
-                    var isMatch = true;
-                    var content_index = [];
-                    var data_title = data.title.trim().toLowerCase();
-                    var data_content = data.content.trim().replace(/<[^>]+>/g, "").toLowerCase();
-                    var data_url = data.url;
-                    var index_title = -1;
-                    var index_content = -1;
-                    var first_occur = -1;
+                    let isMatch = true;
+                    let content_index = [];
+                    let data_title = data.title.trim().toLowerCase();
+                    let data_content = data.content.trim().replace(/<[^>]+>/g, "").toLowerCase();
+                    let data_url = data.url;
+                    let index_title = -1;
+                    let index_content = -1;
+                    let first_occur = -1;
                     // only match artiles with not empty titles and contents
                     if (data_title != '' && data_content != '') {
                         keywords.forEach(function (keyword, i) {
@@ -51,11 +51,11 @@ var searchFunc = function (path, search_id, content_id) {
                     // show search results
                     if (isMatch) {
                         str += "<li><a href='" + data_url + "' class='search-result-title'>" + data_title + "</a>";
-                        var content = data.content.trim().replace(/<[^>]+>/g, "");
+                        let content = data.content.trim().replace(/<[^>]+>/g, "");
                         if (first_occur >= 0) {
                             // cut out 100 characters
-                            var start = first_occur - 20;
-                            var end = first_occur + 80;
+                            let start = first_occur - 20;
+                            let end = first_occur + 80;
                             if (start < 0) {
                                 start = 0;
                             }
@@ -65,10 +65,10 @@ var searchFunc = function (path, search_id, content_id) {
                             if (end > content.length) {
                                 end = content.length;
                             }
-                            var match_content = content.substr(start, end);
+                            let match_content = content.substr(start, end);
                             // highlight all keywords
                             keywords.forEach(function (keyword) {
-                                var regS = new RegExp(keyword, "gi");
+                                let regS = new RegExp(keyword, "gi");
                                 match_content = match_content.replace(regS, "<em class=\"search-keyword\">" + keyword + "</em>");
                             });
 
